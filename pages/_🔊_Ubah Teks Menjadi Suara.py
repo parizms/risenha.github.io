0126c90a5ea6a2232dc179775bba55296584c231
parizms <parizms024@gmail.com>
2023-02-21 13:49:58 +0700

    First commit

---
+++ pages/_🔊_Ubah Teks Menjadi Suara.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Hayoo mau apa")
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
