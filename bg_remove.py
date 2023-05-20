e5b5c4885fe2bc746e0cd181d2fe4a89553b2c10
parizms024 <104338365+parizms@users.noreply.github.com>
2023-05-20 07:57:10 +0700

    Update bg_remove.py

--- bg_remove.py
+++ bg_remove.py
@@ -44,11 +44,3 @@ else:
     fix_image("poto.png")
     
     
-    hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
